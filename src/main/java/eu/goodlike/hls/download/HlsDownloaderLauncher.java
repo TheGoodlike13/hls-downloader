3dde2669aeb610f951567e049188495c6fe0e5ea
Goodlike <henrisssss@hotmail.com>
2016-11-26 21:19:39 +0200

    Setup main method

---
+++ src/main/java/eu/goodlike/hls/download/HlsDownloaderLauncher.java
@@ -0,0 +1,12 @@
+package eu.goodlike.hls.download;
+
+/**
+ * Main class for HlsDownloader
+ */
+public final class HlsDownloaderLauncher {
+
+    public static void main(String... args) {
+
+    }
+
+}

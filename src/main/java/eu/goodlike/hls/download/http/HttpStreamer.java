e3b642da44f2c58a308fa28cefeeac32674202c3
Goodlike <henrisssss@hotmail.com>
2016-11-26 23:38:39 +0200

    Added missing documentation

--- src/main/java/eu/goodlike/hls/download/http/HttpStreamer.java
+++ src/main/java/eu/goodlike/hls/download/http/HttpStreamer.java
@@ -4,8 +4,18 @@ import okhttp3.HttpUrl;
 
 import java.io.InputStream;
 
+/**
+ * Defines how to get an {@link InputStream} containing the contents of any given {@link HttpUrl}
+ */
 public interface HttpStreamer {
 
+    /**
+     * Gets an input stream from any given {@link HttpUrl}
+     *
+     * @param url url to get the contents of
+     * @return {@link InputStream} containing contents of given url
+     * @throws NullPointerException if url is null
+     */
     InputStream getStream(HttpUrl url);
 
 }
--- src/main/java/eu/goodlike/hls/download/http/OkHttpStreamer.java
+++ src/main/java/eu/goodlike/hls/download/http/OkHttpStreamer.java
@@ -3,14 +3,20 @@ package eu.goodlike.hls.download.http;
 import com.google.inject.Inject;
 import com.google.inject.name.Named;
 import eu.goodlike.libraries.okhttp.HttpRequestMaker;
+import eu.goodlike.neat.Null;
 import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
 
 import java.io.InputStream;
 
+/**
+ * Fetches an {@link InputStream} by making a call to {@link OkHttpClient}
+ */
 public final class OkHttpStreamer implements HttpStreamer {
 
     @Override
     public InputStream getStream(HttpUrl url) {
+        Null.check(url).as("url");
         return httpRequestMaker.makeRequest(url).join().body().byteStream();
     }
 

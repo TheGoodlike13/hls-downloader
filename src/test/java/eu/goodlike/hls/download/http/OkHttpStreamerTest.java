7e9eab7ffcfab3500e78f2e28dbb797ec7629d2e
TheGoodlike13 <henrisssss@hotmail.com>
2017-10-07 18:43:36 +0300

    Updated versions

--- src/test/java/eu/goodlike/hls/download/http/OkHttpStreamerTest.java
+++ src/test/java/eu/goodlike/hls/download/http/OkHttpStreamerTest.java
@@ -42,6 +42,7 @@ public class OkHttpStreamerTest {
                 .request(request)
                 .protocol(Protocol.HTTP_1_1)
                 .code(200)
+                .message("OK")
                 .body(responseBody)
                 .build();
     }

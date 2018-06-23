bb3cda59d3fd4a777b5c15f348faf7a54ec46293
TheGoodlike13 <henrisssss@hotmail.com>
2018-06-23 15:01:17 +0300

    Fixed ffmpeg protocol whitelist for HLS files

--- copy/hlsDL.bat
+++ copy/hlsDL.bat
@@ -1,2 +1,2 @@
 @echo off
-java -jar "%~dp0hls-downloader-0.8.jar" %*
+java -jar "%~dp0hls-downloader-0.9.jar" %*
--- src/main/java/eu/goodlike/hls/download/ffmpeg/HlsFfmpegFormatter.java
+++ src/main/java/eu/goodlike/hls/download/ffmpeg/HlsFfmpegFormatter.java
@@ -33,7 +33,7 @@ public final class HlsFfmpegFormatter implements FfmpegFormatter {
 
     private List<String> getFullArgList(String outputFileName, String firstInputFileName, String... inputFileNames) {
         ImmutableList.Builder<String> builder = ImmutableList.builder();
-        builder.addAll(ENSURE_HTTP_ALLOWED_ARGS);
+        builder.addAll(PROTOCOLS_USED_BY_HLS);
         builder.add(INPUT_ARG).add(firstInputFileName);
         Stream.of(inputFileNames).forEach(input -> builder.add(INPUT_ARG).add(input));
         return builder
@@ -42,7 +42,7 @@ public final class HlsFfmpegFormatter implements FfmpegFormatter {
                 .build();
     }
 
-    private static final List<String> ENSURE_HTTP_ALLOWED_ARGS = ImmutableList.of("-protocol_whitelist", "file,tcp,http");
+    private static final List<String> PROTOCOLS_USED_BY_HLS = ImmutableList.of("-protocol_whitelist", "file,tcp,http,https,tls");
     private static final String INPUT_ARG = "-i";
     private static final List<String> DEFAULT_ARGS = ImmutableList.of("-bsf:a", "aac_adtstoasc", "-c", "copy");
 
